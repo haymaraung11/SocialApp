860c9817c421c83f207ce13f9eabc3e55cf862c6
Hay Mar Aung <120264135+haymaraung11@users.noreply.github.com>
2024-02-07 08:00:01 +0630

    Add files via upload

---
+++ socialApp/src/test/java/com/example/javaOJT/SocialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaOJT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
